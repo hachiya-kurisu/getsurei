1a0c92df1f4e4814e9aebbb77e19758bec0d7895
蜂谷栗栖 <kurisu@blekksprut.net>
2025-01-28 01:01:37 +0900

    🙃（figure out formatting later

--- cmd/getsurei/main.go
+++ cmd/getsurei/main.go
@@ -14,6 +14,13 @@ func main() {
 	v := flag.Bool("v", false, "version")
 	f := flag.String("f", "2006.01.02", "date format")
 
+	s := flag.Bool("s", false, "print next new moon")
+	j := flag.Bool("j", false, "print next first quarter moon")
+	m := flag.Bool("m", false, "print next full moon")
+	k := flag.Bool("k", false, "print next last quarter moon")
+
+	g := flag.Bool("g", true, "print corresponding moon phase")
+
 	flag.Parse()
 
 	if *v {
@@ -32,11 +39,34 @@ func main() {
 			fmt.Fprintf(os.Stderr, "failed to parse date: %s\n", raw)
 			continue
 		}
+
+		if *g {
+			fmt.Printf("%s\n", getsurei.Name(date))
+		}
+
 		if *a {
 			days := getsurei.Getsurei(date)
 			fmt.Printf("月齢%.2f\n", days)
-		} else {
-			fmt.Printf("%s\n", getsurei.Name(date))
+		}
+
+		if *s {
+			shingetsu := getsurei.Next(getsurei.Shingetsu, date)
+			fmt.Printf("次の新月：%s\n", shingetsu.Format(*f))
+		}
+
+		if *j {
+			jougen := getsurei.Next(getsurei.Jougen, date)
+			fmt.Printf("次の上限の月：%s\n", jougen.Format(*f))
+		}
+
+		if *m {
+			mangetsu := getsurei.Next(getsurei.Mangetsu, date)
+			fmt.Printf("次の満月：%s\n", mangetsu.Format(*f))
+		}
+
+		if *k {
+			kagen := getsurei.Next(getsurei.Kagen, date)
+			fmt.Printf("次の下弦の月：%s\n", kagen.Format(*f))
 		}
 	}
 }
